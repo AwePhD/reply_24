c510c4ef5a41fa615a8fe4b665cfe97404e93ac2
AwePhD <mathias.reus@gipsa-lab.fr>
2024-03-21 18:14:24 +0100

    oubli

--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,13 @@
-def main():
-    ...
+from reply_24.map import Map
 
+
+def main():
+    map = Map("00-trailer.txt")
+    map = Map("01-comedy.txt")
+    map = Map("02-sentimental.txt")
+    map = Map("03-adventure.txt")
+    map = Map("04-drama.txt")
+    map = Map("05-horror.txt")
 
 if __name__ == "__main__":
     main()
