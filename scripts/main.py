8d280d81ccaacef54a4815593b742a40eacc565e
AwePhD <mathias.reus@gipsa-lab.fr>
2024-03-21 20:30:41 +0100

    last thing

--- scripts/main.py
+++ scripts/main.py
@@ -1,13 +1,19 @@
 from reply_24.map import Map
+from reply_24.solver import Solver
 
 
 def main():
-    map = Map("00-trailer.txt")
-    map = Map("01-comedy.txt")
-    map = Map("02-sentimental.txt")
-    map = Map("03-adventure.txt")
-    map = Map("04-drama.txt")
-    map = Map("05-horror.txt")
+    file_names = [
+        "00-trailer.txt",
+        "01-comedy.txt",
+        "02-sentimental.txt",
+        "03-adventure.txt",
+        "04-drama.txt",
+        "05-horror.txt",
+    ]
+    solver = Solver(Map(file_names[5]))
+    solver.solve()
+
 
 if __name__ == "__main__":
     main()
