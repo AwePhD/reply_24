f8922758248e40a566caedad49af2a034c41fa55
Simard <csimard@allegromicro.com>
2024-03-21 18:55:22 +0100

    add SetTile

--- src/reply_24/map.py
+++ src/reply_24/map.py
@@ -5,15 +5,24 @@ class GoldenPoint(NamedTuple):
     x: int
     y: int
 
+
 class SilverPoint(NamedTuple):
     x: int
     y: int
     score: int
 
+
 class Tile(NamedTuple):
     kind: str
     cost: int
     count: int
+
+
+class SetTile(NamedTuple):
+    tile: Tile
+    x: int
+    y: int
+
 
 class Map:
     # Méthodes:
@@ -25,7 +34,9 @@ class Map:
         with open(in_file, mode="r", encoding="utf-8") as file:
             lines = file.readlines()
 
-        width, height, n_golden_points, n_silver_points, n_tile_types = lines[0].split(" ")
+        width, height, n_golden_points, n_silver_points, n_tile_types = lines[0].split(
+            " "
+        )
         self.width = int(width)
         self.height = int(height)
         self.n_golden_points = int(n_golden_points)
@@ -34,17 +45,34 @@ class Map:
 
         self.golden_points = [
             GoldenPoint(int(line.split(" ")[0]), int(line.split(" ")[1]))
-            for line in lines[1:1+self.n_golden_points]
+            for line in lines[1 : 1 + self.n_golden_points]
         ]
         self.silver_points = [
-            SilverPoint(int(line.split(" ")[0]), int(line.split(" ")[1]), int(line.split(" ")[2]))
-            for line in lines[1+self.n_golden_points:1+self.n_golden_points+self.n_silver_points]
+            SilverPoint(
+                int(line.split(" ")[0]),
+                int(line.split(" ")[1]),
+                int(line.split(" ")[2]),
+            )
+            for line in lines[
+                1
+                + self.n_golden_points : 1
+                + self.n_golden_points
+                + self.n_silver_points
+            ]
         ]
         self.tiles = [
             Tile(line.split(" ")[0], int(line.split(" ")[1]), int(line.split(" ")[2]))
-            for line in lines[1+self.n_golden_points:1+self.n_golden_points+self.n_silver_points]
+            for line in lines[
+                 1
+                + self.n_golden_points
+                + self.n_silver_points:
+            ]
         ]
 
-    def output(self) -> None:
-        ...
+    def tile_dict(self) -> dict[str, Tile]:
+        return {tile.kind: tile for tile in self.tiles}
+
+    def output(self) -> None: ...
+
     ...
+
