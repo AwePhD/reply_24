83285625d6435566f9ad9f4c4af548f3cb42c5b1
AwePhD <mathias.reus@gipsa-lab.fr>
2024-03-21 18:00:22 +0100

    parsing input

--- src/reply_24/map.py
+++ src/reply_24/map.py
@@ -1,24 +1,49 @@
 from typing import NamedTuple
 
 
-# t = Tile(5, 4)
-# t.x -> 5
-class Tile(NamedTuple):
+class GoldenPoint(NamedTuple):
     x: int
     y: int
-    # Add other state
+
+class SilverPoint(NamedTuple):
+    x: int
+    y: int
+    score: int
+
+class Tile(NamedTuple):
+    kind: str
+    cost: int
+    count: int
 
 class Map:
-    # tiles (list[Tile])
-    # gold_tiles (list[Tile])
-    # silver_tiles (list[Tile])
-    # solver (Solver)
     # Méthodes:
     # - A réfléchir: Methode pour faire optimiser la recherche de solution (cache, memoire, deplacement)
+    # ok tile
     # - get_score(self) -> int
     # - sort une solution dans un txt ouput(self) -> None
     def __init__(self, in_file: str) -> None:
-        ...
+        with open(in_file, mode="r", encoding="utf-8") as file:
+            lines = file.readlines()
+
+        width, height, n_golden_points, n_silver_points, n_tile_types = lines[0].split(" ")
+        self.width = int(width)
+        self.height = int(height)
+        self.n_golden_points = int(n_golden_points)
+        self.n_silver_points = int(n_silver_points)
+        self.n_tile_types = int(n_tile_types)
+
+        self.golden_points = [
+            GoldenPoint(int(line.split(" ")[0]), int(line.split(" ")[1]))
+            for line in lines[1:1+self.n_golden_points]
+        ]
+        self.silver_points = [
+            SilverPoint(int(line.split(" ")[0]), int(line.split(" ")[1]), int(line.split(" ")[2]))
+            for line in lines[1+self.n_golden_points:1+self.n_golden_points+self.n_silver_points]
+        ]
+        self.tiles = [
+            Tile(line.split(" ")[0], int(line.split(" ")[1]), int(line.split(" ")[2]))
+            for line in lines[1+self.n_golden_points:1+self.n_golden_points+self.n_silver_points]
+        ]
 
     def output(self) -> None:
         ...
